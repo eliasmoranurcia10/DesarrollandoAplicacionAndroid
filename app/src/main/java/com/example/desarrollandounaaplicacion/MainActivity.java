1c15c49de212e6418c80ddd348f9d9c081a47a9e
EliasMoran <yoexereliasmoranurcia@gmail.com>
2020-07-08 21:10:12 -0500

    Primer caso de calendario; trabajando con la clase estatica DatePickerFragment

--- app/src/main/java/com/example/desarrollandounaaplicacion/MainActivity.java
+++ app/src/main/java/com/example/desarrollandounaaplicacion/MainActivity.java
@@ -1,8 +1,23 @@
 package com.example.desarrollandounaaplicacion;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +25,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+
+    public static class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            //Usar la fecha actual como la fecha por defecto en el picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            //Crear un nueva instancia de DatePickerDialog y retornarlo
+            return new DatePickerDialog(getActivity(),this, year,month,day);
+        }
+
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            //Hacer algunas cosas con la fecha elegida por el usuario
+        }
+    }
+
+    public void showTimePickerDialog(View v){
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),"datePicker");
+
+    }
+
 }
